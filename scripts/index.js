d39b5b2b785dadc86687b55ca42477ecb0fc72a1
Glitch (glitch-hello-website) <none>
2022-02-22 00:55:09 +0000

    🏰☃️ Checkpoint ./index.html:26172174/65 ./scripts/index.js:26172174/41

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Feb 24, 2022',
-  name: 'Tinah'
+  birthdate: 'Feb 24, 9999',
+  name: 'it isnt done yet'
 };
 
 function hideEverything() {
